78565d7c34652344d0e803e4efa9f9a4fee88db8
Mars182838 <wj182838@126.com>
2012-11-06 16:58:18 +0800

    Initial Commit

---
+++ Moving/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Moving
+//
+//  Created by Ibokan on 12-11-6.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
